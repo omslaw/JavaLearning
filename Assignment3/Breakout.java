a05779b084b0fd239a80e039ff49d54395b51dac
omslaw <cafeconleche@overhere.com>
2013-04-23 21:07:27 -0700

    adding my initial stab at the game

--- Assignment3/Breakout.java
+++ Assignment3/Breakout.java
@@ -56,11 +56,98 @@ public class Breakout extends GraphicsProgram {
 
 /** Number of turns */
 	private static final int NTURNS = 3;
+	
+/** The paddle */
+	private GRect paddle;
+	private GObject gobj;   // The object being dragged
+	private GPoint last;    // The last mouse position
 
 /* Method: run() */
 /** Runs the Breakout program. */
-	public void run() {
-		println("test print");
+	public void init() {
+		setup();
+		int x = ((WIDTH - PADDLE_WIDTH) /2);
+		int y = getHeight() - PADDLE_Y_OFFSET;
+		paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setColor(Color.BLACK);
+		add(paddle);
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		last = new GPoint(e.getPoint());
+		gobj = getElementAt(last);
+	}
+	
+	/* Called on mouse drag to reposition the object */
+	public void mouseDragged(MouseEvent e) {
+		if (gobj != null) {
+			double x = e.getX()  - last.getX();
+			println("what is double x" + (e.getX() - PADDLE_WIDTH));
+			println("what is x of object " + gobj.getX());
+			double xLoc = gobj.getX();
+			if (xLoc <= 0) {
+				gobj.move(0, 0);
+			}
+			else if ((xLoc + PADDLE_WIDTH) >= WIDTH) {
+				int edge = WIDTH - PADDLE_WIDTH;
+				gobj.move((WIDTH - PADDLE_WIDTH), 0);
+			} else {
+			gobj.move(x, 0);
+			}
+			last = new GPoint(e.getPoint());
+		}
+	}
+
+
+	/**
+	 * This setups the game board. 
+	 */
+	private void setup() {
+		brickSetup();
+	}
+
+
+	/**
+	 * This setups the bricks at the top of the screen. 
+	 */
+	private void brickSetup() {
+		int x = BRICK_SEP / 2;
+		int y = BRICK_Y_OFFSET;
+		int twoRowHeight = (BRICK_HEIGHT + BRICK_SEP) * 2;
+		createRow(x, y, Color.RED);
+		y += twoRowHeight;
+		createRow(x, y, Color.ORANGE);
+		y += twoRowHeight;
+		createRow(x, y, Color.YELLOW);
+		y += twoRowHeight;
+		createRow(x, y, Color.GREEN);
+		y += twoRowHeight;
+		createRow(x, y, Color.CYAN);
+	}
+
+
+	/**
+	 * Creates two rows of blocks for the game. 
+	 * @param x
+	 * @param y
+	 * @param Color 
+	 */
+	private void createRow(int x, int y, Color color) {
+		for (int i = 1; i <= 2; i++) {
+			for (int j = 1; j <= 10; j++) {
+				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+				brick.setFilled(true);
+				brick.setColor(color);
+				add(brick);
+				x += BRICK_WIDTH + BRICK_SEP;
+			}
+			x = BRICK_SEP / 2;
+			y += BRICK_HEIGHT + BRICK_SEP;
+		}
+		
+
 	}
 
 }
